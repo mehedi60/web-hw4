21c88a7049c2e23ca4f85918ac183cde335446fe
mehedi60 <51970136+mehedi60@users.noreply.github.com>
2019-11-13 00:43:47 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+?>
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Mehedi</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+ <script src="js/function.js" type="text/javascript"> </script>
+</head>
+<body>
+
+<div class="container-fluid">
+
+  <div class="row">
+    <div class="col-sm-2" style="background-color:#aaaaaa; height: 80px ;opacity: .7"><img src="img/LogoMakr_61haj6.png" width="200px"   ></div>
+      <div class="col-sm-10" style="background-color:#aaaaaa; height: 80px ;opacity: .5">
+      
+      <ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" href="#"><p><b>Home</b></p></a>
+  </li>
+  <li class="nav-item dropdown" >
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Women Dress</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Link 1</a><br>
+      <a class="dropdown-item" href="#">Link 2</a><br>
+      <a class="dropdown-item" href="#">Link 3</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Men Dress</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Link 1</a><br>
+      <a class="dropdown-item" href="#">Link 2</a><br>
+      <a class="dropdown-item" href="#">Link 3</a>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Store</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="#"><b>About</b></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="signup.php">Sign up</a>
+  </li>
+  
+</ul>
+      
+      
+      </div>
+    
+  </div>
+     <div class="row">
+    
+             <div class="col-sm-12" style="background-color:#aaaaaa; opacity: .6;height: 350px;"> </div>
+    </div>
+    
+     <div class="row">
+    
+             <div class="col-sm-12" style="background-color:white; opacity: .6;height: 150px;"></div>
+             
+    </div>
+    
+    
+    <div class="row">
+    
+             <div class="col-sm-12" style="background-color:white; height: 150px;"> </div>
+             
+             
+</div>
+    </div>
+    <footer class="container-fluid text-center" style="background-color:#aaaaaa; height:100px;">
+ 
+</footer>
+    </body>
+</html>
+
+
+    
+    
+    
